6cbbbef8367cb466ba4a14d0f7113e7ef72c8c6b
Ivo Juarez <75588359+Vanzo93@users.noreply.github.com>
2024-04-07 06:43:35 -0300

    Funcionando correctamente el panel admin tanto con Proveedores como Producto

--- compra/admin.py
+++ compra/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Proveedor, Producto
 
-# Register your models here.
+
+# Panel de ADMIN
+
+class ProveedorAdmin(admin.ModelAdmin):
+    # 1° propiedad: Permite que aparezcan los nombres de las columnas de los productos.
+    # Sin esto, cada item de la clase producto se vería como "Producto Object(1)"
+    list_display = ['id', 'nombre', 'apellido', 'dni']
+    # 2° propiedad: Agrega un cuadro de búsqueda, dependiendo de lo que se ponga dentro del search_fields,
+    # buscará por determinada característica.
+    search_fields = ['id', 'nombre', 'apellido', 'dni']
+
+
+# Lo mismo hago para Producto
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nombre', 'precio', 'stock_actual', 'proveedor']
+    search_fields = ['id', 'nombre', 'precio', 'stock_actual']
+
+
+# Agrego la línea de código para vincular los modelos con el Admin
+admin.site.register(Proveedor, ProveedorAdmin)
+admin.site.register(Producto, ProductoAdmin)
+
--- compra/models.py
+++ compra/models.py
@@ -5,6 +5,11 @@ class Proveedor(models.Model):
     nombre = models.CharField(max_length=60)
     apellido = models.CharField(max_length=60)
     dni = models.IntegerField(max_length=10)
+    # Quiero que figure no solamente el nombre, sino también el apellido del proveedor
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
+
+        
 
 
 # Creamos modelo PRODUCTO en donde tiene su campo PROVEEDOR dependiendo de la clase anterior mencionada
@@ -13,4 +18,5 @@ class Producto(models.Model):
     precio = models.FloatField(max_length=15)
     stock_actual = models.IntegerField(max_length=10)
     proveedor = models.ForeignKey(Proveedor, on_delete=models.CASCADE)
-    
+    def __str__(self):
+        return self.nombre
